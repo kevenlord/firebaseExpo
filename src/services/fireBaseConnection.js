08dfdaade072d30dee87f0328f597bf6a1c6095d
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-06-06 10:09:26 -0300

    fix

--- src/services/fireBaseConnection.js
+++ src/services/fireBaseConnection.js
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+/* 
 const firebaseConfig = {
     apiKey: "AIzaSyCt107Jv4JGOK6eb_xRp22d6kCmyqjvtIg",
     authDomain: "expofirebase-39a4e.firebaseapp.com",
@@ -14,7 +15,16 @@ const firebaseConfig = {
     messagingSenderId: "730152178088",
     appId: "1:730152178088:web:a6586b947f53f075fbcd9d"
 };
-
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyBowv4KLiILW_-27sOEiDkg00G5o2XVPf4",
+    authDomain: "xepa-8540f.firebaseapp.com",
+    projectId: "xepa-8540f",
+    storageBucket: "xepa-8540f.appspot.com",
+    messagingSenderId: "328331711092",
+    appId: "1:328331711092:web:4891b9afc345eed4adb721",
+    measurementId: "G-J8R7WM87FH"
+  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); 
